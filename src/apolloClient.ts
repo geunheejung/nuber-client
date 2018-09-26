ec8ae00cd29171f2f1ad9947099517bf89f09dc6
geunheejung <geuni1013@gmail.com>
2018-09-26 23:55:43 +0900

    apollo client에 모든 request에 대한 hook 추가

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,6 +1,20 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { Operation } from 'apollo-boost';
 
 const client = new ApolloClient({
+  request: async (operation: Operation) => {
+    // graphql에서 query를 request하기전에 가로챌 수 있다.
+
+    const getContextConfig = () => {
+      const XJWT = 'X-JWT';
+      return {
+        headers: {
+          [XJWT]: localStorage.getItem(`jwt`) || '',
+        },
+      };
+    };
+
+    operation.setContext(getContextConfig());
+  },
   uri: 'http://localhost:4000/graphql',
 });
 
