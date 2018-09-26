141d9ed90f67656d4c87565074ac315ea4eaf210
geunheejung <geuni1013@gmail.com>
2018-09-26 23:47:20 +0900

    apollo react에 주입

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import apolloClient from './apolloClient';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render(
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root') as HTMLElement
+);
