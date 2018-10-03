5a760a82495bd35446437e16b5355c350106ff28
geunheejung <gh.jung1@albamapp.com>
2018-10-03 16:27:33 +0900

    styled-componets 적용

--- src/components/App/AppContainer.tsx
+++ src/components/App/AppContainer.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import reset from 'styled-reset';
 import { IS_LOGGED_IN } from "./AppQueries";
+import { injectGlobal, ThemeProvider } from '../../typed-componets';
+import theme from '../../theme';
 import AppPresenter from './AppPresenter';
+
+// tslint:disable-next-line
+injectGlobal`
+    ${reset}
+`;
 
 const AppContainer = (props) => {
     console.log(props);
     return (
-        <AppPresenter isLoggedIn={props.data.auth.isLoggedIn}/>
+        <ThemeProvider theme={theme}>
+            <AppPresenter isLoggedIn={props.data.auth.isLoggedIn}/>
+        </ThemeProvider>
     )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+// // theme.ts
+// export default interface ThemeInterface {
+//   primaryColor: string;
+//   primaryColorInverted: string;
+// }
+
+const theme = {
+  blueColor: '#3498db',
+}
+
+export default theme;
